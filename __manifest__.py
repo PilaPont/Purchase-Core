50bf325ee25f1f8545d876d9f7d1f33a2c1337eb
Maryam Kia <maryam.kia1984@mail.com>
2021-07-05 18:59:51 +0430

    [IMP] purchase_core, monetary widget for price_unit field in view and report

--- __manifest__.py
+++ __manifest__.py
@@ -1,9 +1,10 @@
 {
     'name': 'Purchase Core',
-    'version': '14.0.0.1.2.0+210613',
+    'version': '14.0.0.1.1.1+210705',
     'description': """
      This module modifies some properties of Purchase, including:
-    - Converts original CRM demo and data records into Farsi.
+            _ Convert original purchase demo and data records into Farsi
+            _ Add monetary widget to price_unit
     """,
     'author': "Kenevist Developers, Maryam Kia",
     'website': "https://www.kenevist.ir",
@@ -11,8 +12,10 @@
     'category': 'Localization',
     'depends': ['purchase'],
     'data': [
+        'views/purchase_order_views.xml',
         'data/mail_template_data.xml',
         'data/purchase_data.xml',
+        'report/purchase_order_templates.xml',
     ],
     'demo': [
         'data/purchase_demo.xml',
